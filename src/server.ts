5c58c9ba25b8335ad5ec90a05205197d2ccaad15
Celso Santos <celsovcsantos@gmail.com>
2023-04-13 11:52:27 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+/* 
+npm init -y
+npm install typescript ts-node-dev @types/node tsconfig-paths -D
+npx tsc --init --rootDir src --outDir build --esModuleInterop --resolveJsonModule --lib es6 --module commonjs --allowJs true --noImplicitAny true
+*/
+
+console.log("Hello Dev!")
+
