8880caf237965ec7e073e37c2dca56c3653e83cc
csantosnovaenergia <csantos@novaenergia.com.br>
2023-05-02 13:46:38 -0300

    até aula 64

---
+++ src/modules/users/controllers/ResetPasswordController.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from 'express';
+import ResetPasswordService from '../services/ResetPasswordService';
+
+export default class ResetPasswordController {
+  public async create(req: Request, res: Response): Promise<Response> {
+    const { token, password } = req.body;
+    const resetPasswortdEmail = new ResetPasswordService();
+    await resetPasswortdEmail.execute({ token, password });
+    return res.status(204).json();
+  }
+}
